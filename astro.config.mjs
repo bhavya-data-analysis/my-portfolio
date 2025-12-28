432a832ca380dea7c26b7bab60d9c2f972f91248
Bhavya <bhavyapandya005@gmail.com>
2025-12-28 18:28:25 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://my-portfolio-kvzu.vercel.app",
   vite: {
     plugins: [tailwindcss()]
   }
